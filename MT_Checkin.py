ecca6bc446c12a917da897da0d23cc266989a5d2
celetor <86599970+celetor@users.noreply.github.com>
2023-03-24 10:58:42 +0800

    Update MT_Checkin.py

--- MT_Checkin.py
+++ MT_Checkin.py
@@ -7,8 +7,7 @@ QYWX_KEY = os.environ.get('QYWX_KEY')
 
 HEADERS = {
     'authority': 'bbs.binmt.cc',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;'
-              'q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
     'referer': 'https://bbs.binmt.cc/member.php?mod=logging&action=login&mobile=2',
     'sec-fetch-dest': 'document',
@@ -16,8 +15,7 @@ HEADERS = {
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
-                  'Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/110.0.0.0',
+    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/110.0.0.0',
 }
 
 
